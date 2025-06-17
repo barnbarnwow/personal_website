97a8e06f6268ce5c740f5f61584aacecfcf1f33a
Baranpas Jesse <baranpasjesse@Baranpass-MacBook-Air.local>
2025-06-16 19:09:27 -0500

    please work3

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        href="https://fonts.googleapis.com/css2?family=Ballet:opsz@16..72&family=Bodoni+Moda+SC:ital,opsz,wght@0,6..96,400..900;1,6..96,400..900&display=swap"
+        rel="stylesheet"
+      />
       <body className={`${roboto_flex.className} antialiased`}>{children}</body>
     </html>
   );
