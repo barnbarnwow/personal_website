10efaf9760e329dc7c5e114c41dc5c0c7b437d2c
barney <barneyjesse@gmail.com>
2025-11-23 19:49:40 -0600

    navbar links are iterated components from list

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,98 +2,49 @@
 import { useState } from "react";
 import Link from "next/link";
 
+const navigationLinks = [
+  { name: "Home", href: "/" },
+  { name: "Projects", href: "/projects" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+];
+
+const desktopNav = (
+  <nav className="hidden md:flex space-x-4">
+    {navigationLinks.map((link) => (
+      <Link
+        key={link.name}
+        href={link.href}
+        className="hover:underline"
+      >
+        {link.name}
+      </Link>
+    ))}
+  </nav>
+);
+
+const mobileNav = (
+  <nav className="md:hidden">
+    {navigationLinks.map((link) => (
+      <Link
+        key={link.name}
+        href={link.href}
+      >
+        {link.name}
+      </Link>
+    ))}
+  </nav>
+);
+
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <header className="md:shadow-sm">
-      {/* Navbar */}
-      <nav className="w-full flex items-center justify-between md:justify-center">
-        <div>
-          {/* Logo */}
-          <Link href="/">
-            <img
-              src="/logo.png"
-              alt=""
-              className="w-15 h-15 md:w-15 md:h-15 object-contain rounded-full m-3 md:mr-20"
-            />
-          </Link>
-        </div>
-
-        {/* Desktop nav */}
-        <ul className="hidden md:flex text-xl space-x-4">
-          <li>
-            <Link
-              href="/"
-              className="relative inline-block duration-100 after:absolute after:left-0 after:bottom-0 after:h-[1px] after:w-0 after:bg-red-500 after:transition-all after:duration-100 hover:after:w-full"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/about"
-              className="relative inline-block duration-100 after:absolute after:left-0 after:bottom-0 after:h-[1px] after:w-0 after:bg-red-500 after:transition-all after:duration-100 hover:after:w-full"
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/projects"
-              className="relative inline-block duration-100 after:absolute after:left-0 after:bottom-0 after:h-[1px] after:w-0 after:bg-red-500 after:transition-all after:duration-100 hover:after:w-full"
-            >
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/contact"
-              className="relative inline-block duration-100 after:absolute after:left-0 after:bottom-0 after:h-[1px] after:w-0 after:bg-red-500 after:transition-all after:duration-100 hover:after:w-full"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-
-        {/* Mobile toggle button */}
-        <button
-          className="md:hidden mx-2 p-2 bg-red-500"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? (
-            <img src="/close_icon.svg" alt="Close" width={28} height={28} />
-          ) : (
-            <img src="/menu_icon.svg" alt="Menu" width={28} height={28} />
-          )}
-        </button>
-      </nav>
-
-      {/* Mobile nav, appears below nav row */}
-      {isOpen && (
-        <ul className="md:hidden bg-red-500 text-xl p-6 m-4 animate-slideDownFade">
-          <li>
-            <Link href="/" className="block hover:underline">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" className="block hover:underline">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href="/projects" className="block hover:underline">
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact" className="block hover:underline">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      )}
+    <header className="bg-transparent p-4 flex flex-col md:flex-row md:justify-around items-center">
+      <div className = "items-center text-center">
+        <img src="/logo.png" alt="JPOINT Logo" className="h-15 w-15 rounded-full" />
+        <div className="font-bold">J-POINT</div>
+      </div>
+      {desktopNav}
+      {mobileNav}
     </header>
   );
 }
